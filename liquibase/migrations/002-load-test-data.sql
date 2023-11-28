75802a9bb143244dd435bde3cfd6ed0e48cf13d2
emina-mahmutbegovic <eminamahmutbey@gmail.com>
2023-11-28 16:12:30 +0100

    Update test data

--- liquibase/migrations/002-load-test-data.sql
+++ liquibase/migrations/002-load-test-data.sql
@@ -2,11 +2,10 @@
 
 --changeset emina.mahmutbegovic:load_test_data
 
-COPY termin_emitovanja FROM '/liquibase/test-data/tv-date-test-data.csv' WITH CSV HEADER DELIMITER ',';
-COPY urednik FROM '/liquibase/test-data/tv-editor-test-data.csv' WITH CSV HEADER DELIMITER ',';
-COPY epizoda FROM '/liquibase/test-data/tv-episode-test-data.csv' WITH CSV HEADER DELIMITER ',';
-COPY vrsta_emisije FROM '/liquibase/test-data/tv-genre-test-data.csv' WITH CSV HEADER DELIMITER ',';
-COPY gost FROM '/liquibase/test-data/tv-guest-test-data.csv' WITH CSV HEADER DELIMITER ',';
-COPY voditelj FROM '/liquibase/test-data/tv-host-test-data.csv' WITH CSV HEADER DELIMITER ',';
-COPY emisija FROM '/liquibase/test-data/tv-show-test-data.csv' WITH CSV HEADER DELIMITER ',';
-
+COPY urednik FROM '/test-data/tv-editor-test-data.csv' WITH CSV HEADER DELIMITER ',';
+COPY vrsta_emisije FROM '/test-data/tv-genre-test-data.csv' WITH CSV HEADER DELIMITER ',';
+COPY gost FROM '/test-data/tv-guest-test-data.csv' WITH CSV HEADER DELIMITER ',';
+COPY voditelj FROM '/test-data/tv-host-test-data.csv' WITH CSV HEADER DELIMITER ',';
+COPY emisija FROM '/test-data/tv-show-test-data.csv' WITH CSV HEADER DELIMITER ',';
+COPY epizoda FROM '/test-data/tv-episode-test-data.csv' WITH CSV HEADER DELIMITER ',';
+COPY termin_emitovanja FROM '/test-data/tv-date-test-data.csv' WITH CSV HEADER;
